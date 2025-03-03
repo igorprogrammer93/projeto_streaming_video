e50eb23e424855d4b3ba838ee0554160a3eae0b4
igorprogrammer93 <igormtba@gmail.com>
2025-03-03 17:56:48 -0300

    Mensagem do commit

---
+++ Hello_Word_Java/HelloWorld.java
@@ -0,0 +1,10 @@
+// Nome do arquivo: HelloWorld.java
+
+// Definição da classe pública
+public class HelloWorld {
+    // Método principal: ponto de entrada do programa
+    public static void main(String[] args) {
+        // Exibe a mensagem no console
+        System.out.println("Hello, World!");
+    }
+}
